abcf4f6ea3f714ff9a09bcd8ff113a055ca4f1b0
Ryan Clarke <ryan@ryanclarke.net>
2016-03-05 21:33:41 -0500

    Parse hash into list of names and dates

--- tenure.rkt
+++ tenure.rkt
@@ -1,7 +1,16 @@
 #lang racket
 
-(require net/http-client json)
+(require json
+         net/http-client
+         racket/date)
+(date-display-format 'iso-8601)
 (define-values (status header response)
   (http-sendrecv "app.net.sep.com" "/hired"))
 (define data (read-json response))
-(printf "~a~%" data)
+(define (pp h)
+  (for/list ([(k v) (in-hash h)])
+             (values (list k
+                           (hash-ref v 'hiredate)
+                           (date->string (current-date))))))
+(define output (pp data))
+(printf "~a~%" output)
