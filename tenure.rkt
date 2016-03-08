ae100ee328034c1ce9d24ae5c936560fa0ed3b53
Ryan Clarke <ryan@ryanclarke.net>
2016-03-08 17:26:15 -0500

    Refactor json parsing

--- tenure.rkt
+++ tenure.rkt
@@ -5,31 +5,32 @@
          racket/date)
 (date-display-format 'iso-8601)
 
+(define default-date "2001-01-01")
+(define today (date->string (current-date)))
 
 (define (get-response uri)
   (define-values (status header response)
     (http-sendrecv "app.net.sep.com" uri))
   (read-json response))
 
+(define (get-date hash property)
+  (hash-ref hash property today))
+(define (good-date? property)
+  (equal? default-date property))
+
+(define (normalize h)
+  (for/list (((k v) (in-hash h))
+             #:unless (or (good-date? (get-date v 'hiredate))
+                          (good-date? (get-date v 'enddate))))
+    (list k
+          (get-date v 'hiredate)
+          (get-date v 'enddate))))
+
 (define hired-data (get-response "/hired"))
 (define alum-data (get-response "/alums"))
 
-(define (hired-parse h)
-  (for/list (((k v) (in-hash h))
-             #:unless (equal? "2001-01-01" (hash-ref v 'hiredate)))
-    (list k
-          (hash-ref v 'hiredate)
-          (date->string (current-date)))))
-(define hired-output (hired-parse hired-data))
-
-(define (alum-parse h)
-  (for/list (((k v) (in-hash h))
-             #:unless (or (equal? "2001-01-01" (hash-ref v 'hiredate))
-                          (equal? "2001-01-01" (hash-ref v 'enddate))))
-    (list k
-          (hash-ref v 'hiredate)
-          (hash-ref v 'enddate))))
-(define alum-output (alum-parse alum-data))
+(define hired-output (normalize hired-data))
+(define alum-output (normalize alum-data))
 
 (define output (append hired-output alum-output))
 (printf "~a~%" output)
