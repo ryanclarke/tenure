bccda3155049b3db8174c69014c8270261105668
Ryan Clarke <ryan@ryanclarke.net>
2016-03-08 22:16:36 -0500

    Show people with duration is years

--- tenure.rkt
+++ tenure.rkt
@@ -5,8 +5,20 @@
          racket/date)
 (date-display-format 'iso-8601)
 
+(define (dump x) (printf "~a~%" x))
+
+(define (string->date date)
+  (define ymd-string-list (string-split date "-" #:repeat? #t))
+  (define ymd-nums (for/list ((n ymd-string-list))
+                     (string->number n)))
+  (define dmy-nums (reverse ymd-nums))
+  (seconds->date (apply find-seconds 0 0 0 dmy-nums)))
 (define default-date "2001-01-01")
 (define today (date->string (current-date)))
+(define (years-between hired end)
+  (exact->inexact (abs (/ (- (date->seconds hired)
+                        (date->seconds end))
+                     31536000))))
 
 (define (get-response uri)
   (define-values (status header response)
@@ -14,7 +26,7 @@
   (read-json response))
 
 (define (get-date hash property)
-  (hash-ref hash property today))
+  (string->date(hash-ref hash property today)))
 (define (good-date? property)
   (equal? default-date property))
 
@@ -32,5 +44,12 @@
 (define hired-output (normalize hired-data))
 (define alum-output (normalize alum-data))
 
-(define output (append hired-output alum-output))
-(printf "~a~%" output)
+(define all (append hired-output alum-output))
+
+(define (person-duration l)
+  (for/list (((x) (in-list l)))
+      (list (first x)
+            (years-between (third x) (second x)))))
+ 
+(dump (person-duration all))
+
